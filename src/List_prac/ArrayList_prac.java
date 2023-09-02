c16e21eb23267580aef68c454b73fe396f5b8aed
Zaheeraz <zaheeraz730@gmail.com>
2023-09-02 13:00:25 +0530

    First Commit

---
+++ src/List_prac/ArrayList_prac.java
@@ -0,0 +1,52 @@
+package List_prac;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class ArrayList_prac {
+
+	public static void main(String[] args) {
+
+//		ArrayList<String> list = new ArrayList<String>();
+//		list.add("Zaheer");
+//		list.add("Ahmed");
+//		list.add("Nazeer");
+//		list.add("vijay");
+//		
+//		System.out.println(list);
+//		
+//		System.out.println(list.get(1));
+//		
+//		//itrable 
+//		
+//		Iterator itr = list.iterator();
+//		while(itr.hasNext()) {
+//			System.out.println(itr.next());
+//		}
+//		
+//		//listIterator
+//		System.out.println("======================");
+//		ListIterator<String> lst = list.listIterator(list.size());
+//		while(lst.hasPrevious()) {
+//			String s = lst.previous();
+//			System.out.println(s);
+//		}
+//		
+//		System.out.println("#################");
+//		list.forEach(a->{
+//			System.out.println(a);
+//		});
+//		
+//		ArrayList<String> list1 = new ArrayList<String>(Arrays.asList("siva","kumar"));
+//		System.out.println(list1);
+		
+	
+	char[] ch = {'m','o','m'};
+	
+	String s = new String(ch);
+	System.out.println(s);
+	}
+
+}
